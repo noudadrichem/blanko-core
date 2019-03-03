4dc8f35dfeb412b597d7acccacc5cb6121a3f5c5
noudadrichem <info@noudadrichem.com>
2019-03-03 15:12:52 +0100

    rebuild task

---
+++ src/services/tasks/index.js
@@ -0,0 +1,33 @@
+import { Router as router } from 'express'
+import Task from '../../models/tasks'
+import log from '../../log'
+import { authenticate } from '../../middlewares/auth'
+import Account from '../../models/account'
+import Project from '../../models/projects'
+
+import tasksActions from './actions'
+const {
+  getAllTasks,
+  getSingleTask,
+  addTask,
+  updateSingleTask,
+  deleteTask,
+  updateSubTask,
+  deleteSubTask,
+  updateSubTaskStatus,
+} = tasksActions({ Account, Task, Project, log })
+
+export default function tasksController() {
+  const tasks = router()
+
+  tasks.get('/', authenticate, getAllTasks)
+  tasks.get('/:taskId', authenticate, getSingleTask)
+  tasks.post('/add/', authenticate, addTask)
+  tasks.put('/update/:taskId', authenticate, updateSingleTask)
+  tasks.delete('/:taskId', authenticate, deleteTask)
+  tasks.put('/sub/:taskId', authenticate, updateSubTask)
+  tasks.put('/delsub/:projectId/:taskId/:subTaskId', authenticate, deleteSubTask)
+  tasks.put('/updatesub/:projectId/:taskId/:subTaskId', authenticate, updateSubTaskStatus)
+
+  return tasks
+}
